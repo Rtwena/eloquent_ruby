ad457ab8ce5742da4470f2a4c5ad431a616b057a
Richard Twena <Richardtwena@gmail.com>
2015-03-03 10:16:55 +0000

    QuickStringArray

--- eloquent_ruby.rb
+++ eloquent_ruby.rb
@@ -1,9 +1,15 @@
 module EloquentRuby
+
   class FancyIfElse
     #fancy if/else
     #if morning is true then 'good morning', else 'good evening'
     morning = true
     greetings = morning ? 'Good morning!' : 'Good Evening!'
     puts greetings
+  end
+
+  class QuickStringArray
+    str_array = %w{ this is a quick way to create an array of strings!}
+    str_array.each { |str| puts str }
   end
 end
